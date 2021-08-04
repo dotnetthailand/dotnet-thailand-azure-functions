11e8ff597fa21bcd597947df89b85bc0a0f0d8c1
aaronamm <theeranitp@gmail.com>
2021-08-04 20:44:35 +0700

    Update to use UA

--- facebook-webhook/index.ts
+++ facebook-webhook/index.ts
@@ -1,5 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import axios from 'axios';
+import { uuid } from 'uuidv4';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
@@ -51,13 +52,13 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
   // Event value must be numeric.
   try {
-    await trackEvent(
+    const response = await trackEvent(
       'Example category',
       'Example action',
       'Example label',
       100
     );
-    console.log('event sent');
+    console.log(JSON.stringify(response.data, null, 2));
     // https://github.com/Azure/azure-functions-nodejs-worker/blob/v2.x/src/http/Response.ts#L8
     context.res = {
       headers: {
@@ -87,17 +88,18 @@ function trackEvent(category: string, action: string, label: string, value: numb
 
   // The following environment variable is set by app.yaml when running on App
   // Engine, but will need to be set manually when running locally. See README.md.
-  const { GA_TRACKING_ID } = process.env;
-  console.log(`GA_TRACKING_ID ${GA_TRACKING_ID}`);
+  const { UA_TRACKING_ID } = process.env;
+  console.log(`UA_TRACKING_ID ${UA_TRACKING_ID}`);
 
   const data: Record<string, string> = {
     // API Version.
-    v: '1',
+    // https://www.youtube.com/watch?v=jRmb0jMNUKU We need UA
+    v: '1', // https://www.simoahava.com/analytics/implementation-guide-events-google-analytics-4/
     // Tracking ID / Property ID.
-    tid: GA_TRACKING_ID,
+    tid: UA_TRACKING_ID,
     // Anonymous Client Identifier. Ideally, this should be a UUID that
     // is associated with particular user, device, or browser instance.
-    cid: '555',
+    cid: uuid(),
     // Event hit type.
     t: 'event',
     // Event category.
@@ -108,18 +110,28 @@ function trackEvent(category: string, action: string, label: string, value: numb
     el: label,
     // Event value.
     ev: value.toString(),
+    ua: 'azure-functions'
   };
 
-  const config = {
-    headers: {
-      // We need to use form-url encode https://github.com/openiddict/openiddict-core/issues/437
-      // https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  };
+  // const config = {
+  //   headers: {
+  //     // We need to use form-url encode https://github.com/openiddict/openiddict-core/issues/437
+  //     // https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   }
+  // };
 
   // Request token
-  return axios.post('http://www.google-analytics.com/collect', new URLSearchParams(data), config);
+  // https://www.google-analytics.com/collect
+
+
+  // For debuging your requests you can use this URL:
+  // /debug/collect
+  // instead of
+  // /collect
+  // In response body you will see details.
+
+  return axios.post('https://www.google-analytics.com/collect', new URLSearchParams(data));
 };
 
 
