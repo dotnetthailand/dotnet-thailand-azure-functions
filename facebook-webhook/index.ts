1513a7d45a642ac7c7695e49ea064d0efa40df2b
aaronamm <theeranitp@gmail.com>
2021-08-04 19:05:16 +0700

    Push event to Google Analytics

--- facebook-webhook/index.ts
+++ facebook-webhook/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import axios from 'axios';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
@@ -9,7 +10,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   const challenge = req.query['hub.challenge'] ?? '';
   console.log(`mode: ${mode}, token: ${token}, challenge: ${challenge}`);
   context.log(`body: ${req.body}`);
-
 
   // comment
   // {
@@ -49,15 +49,78 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   // }
 
 
-  // https://github.com/Azure/azure-functions-nodejs-worker/blob/v2.x/src/http/Response.ts#L8
-  context.res = {
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8'
-    },
-    statusCode: 200,
-    body: challenge // We need to send challenge back unless it won't pass Facebook verification when we add callback URL.
-  };
+  // Event value must be numeric.
+  try {
+    await trackEvent(
+      'Example category',
+      'Example action',
+      'Example label',
+      100
+    );
+    console.log('event sent');
+    // https://github.com/Azure/azure-functions-nodejs-worker/blob/v2.x/src/http/Response.ts#L8
+    context.res = {
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
+      statusCode: 200,
+      body: challenge // We need to send challenge back unless it won't pass Facebook verification when we add callback URL.
+    };
+
+  } catch (error) {
+    // This sample treats an event tracking error as a fatal error. Depending
+    // on your application's needs, failing to track an event may not be
+    // considered an error.
+    console.log(error);
+    context.res = {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      statusCode: 500,
+      body: error
+    };
+  }
 
 };
+
+function trackEvent(category: string, action: string, label: string, value: number) {
+
+  // The following environment variable is set by app.yaml when running on App
+  // Engine, but will need to be set manually when running locally. See README.md.
+  const { GA_TRACKING_ID } = process.env;
+  console.log(`GA_TRACKING_ID ${GA_TRACKING_ID}`);
+
+  const data: Record<string, string> = {
+    // API Version.
+    v: '1',
+    // Tracking ID / Property ID.
+    tid: GA_TRACKING_ID,
+    // Anonymous Client Identifier. Ideally, this should be a UUID that
+    // is associated with particular user, device, or browser instance.
+    cid: '555',
+    // Event hit type.
+    t: 'event',
+    // Event category.
+    ec: category,
+    // Event action.
+    ea: action,
+    // Event label.
+    el: label,
+    // Event value.
+    ev: value.toString(),
+  };
+
+  const config = {
+    headers: {
+      // We need to use form-url encode https://github.com/openiddict/openiddict-core/issues/437
+      // https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  // Request token
+  return axios.post('http://www.google-analytics.com/collect', new URLSearchParams(data), config);
+};
+
 
 export default httpTrigger;
